41e18870a841694e2c9b468f4c8f936de7dc0820
hansand02 <91554059+hansand02@users.noreply.github.com>
2024-09-10 10:37:43 +0200

    fix: make countdown timer a background thread, use time.sleep for waiting logic

--- ideas.md
+++ ideas.md
@@ -1,3 +1,4 @@
 ### Progress bar
 ### Make an extension / visible gui thing
 ### Learn about back ergonometrics to have correct sitting/standing ratio.
+### Monitor standing engagement over time. Gamify / give fitness level.
--- venv/test.py
+++ venv/test.py
@@ -2,29 +2,27 @@
 from DeskController import DeskController
 import time
 import countdown
-
+import threading
 
 def up_and_down_alert(controller: DeskController):
     base_height = controller.get_current_height()
     for i in range(3):
         controller.move_to_position((base_height + 10) if i % 2 == 0 else (base_height))
 
-def pomodoro_timer(controller: DeskController, sessions: int, firstType: str = 'stand'):
-    if firstType not in ['stand', 'sit']:
-        raise ValueError("firstType must be either 'stand' or 'sit'")
-    
-    working_time = "25m"
-    pause_time = "5m"
-    positions = ['stand', 'sit'] if firstType == 'stand' else ['sit', 'stand']
-    for i in range(sessions):
-        nextMove = positions[i%2]
-        print("Moving to", nextMove)
-        controller.move_to_position(nextMove)
-        controller.terminal_countdown(working_time)
+def pomodoro_timer(controller: DeskController, sessions: int):
 
+    
+    pause_time = "5m"
+    for i in range(sessions):
+        total_time = "25m"  # Convert standing and sitting time to seconds
+        countdown_thread = threading.Thread(target=controller.terminal_countdown, args=(total_time,))
+        countdown_thread.start()
+        controller.move_to_position('stand')
+        time.sleep(12 * 60)  # Wait for standing time
+        controller.move_to_position('sit')
+        time.sleep(13 * 60)  # Wait for sitting time
         controller.move_to_position('pause')
         controller.terminal_countdown(pause_time)
-        time.sleep(pause_time)
 
 
 if __name__ == "__main__":
